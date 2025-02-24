c13bf47cfbefa690197f12f03c0c85a37330e01f
M. R. Rafsanjani <74598357+rafsanlab@users.noreply.github.com>
2025-02-24 15:01:38 +0000

    multiple edits
    - datafiles.py: edit get_paths_ftype()
    - add init files
    - rename folders

---
+++ messylib/commons/datafiles.py
@@ -0,0 +1,29 @@
+import pathlib, os
+from natsort import natsorted
+
+# --------------------------------------------------------------------
+
+def get_paths_ftype(
+      target_path:str,
+      filetype:str,
+      exclude_hidden:bool=True,
+      make_string:bool=True,
+      verbose:bool=True,
+      ) -> list:
+    """
+    Get paths from a directory with spesific file type.
+
+    """
+    target_path = pathlib.Path(target_path)
+    paths = list(target_path.glob(f"*{filetype}"))
+    if exclude_hidden:
+        paths = [p for p in paths if not os.path.basename(p).startswith('.')]
+    if make_string:
+        paths = [str(p) for p in paths]
+    paths = natsorted(paths)
+    if verbose == True:
+        print(f'Total paths: {len(paths)}')
+    return paths
+
+# --------------------------------------------------------------------
+
