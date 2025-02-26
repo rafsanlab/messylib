2a5a9c6b1fd7a59c37659d92f962eabea47bef1f
M. R. Rafsanjani <74598357+rafsanlab@users.noreply.github.com>
2025-02-26 00:55:59 +0000

    Update files.py
    using python black formatter

--- messylib/commons/files.py
+++ messylib/commons/files.py
@@ -3,13 +3,14 @@ from natsort import natsorted
 
 # --------------------------------------------------------------------
 
+
 def get_paths_filetype(
-      target_path:str,
-      filetype:str,
-      exclude_hidden:bool=True,
-      make_string:bool=True,
-      verbose:bool=True,
-      ) -> list:
+    target_path: str,
+    filetype: str,
+    exclude_hidden: bool = True,
+    make_string: bool = True,
+    verbose: bool = True,
+) -> list:
     """
     Get paths from a directory with spesific file type.
 
@@ -17,13 +18,13 @@ def get_paths_filetype(
     target_path = pathlib.Path(target_path)
     paths = list(target_path.glob(f"*{filetype}"))
     if exclude_hidden:
-        paths = [p for p in paths if not os.path.basename(p).startswith('.')]
+        paths = [p for p in paths if not os.path.basename(p).startswith(".")]
     if make_string:
         paths = [str(p) for p in paths]
     paths = natsorted(paths)
     if verbose == True:
-        print(f'Total paths: {len(paths)}')
+        print(f"Total paths: {len(paths)}")
     return paths
 
-# --------------------------------------------------------------------
 
+# --------------------------------------------------------------------
